c3a9ee9d55ae0c39c79d98e195b7c9dc4680e5f8
Kevin Tun <kevintun01@gmail.com>
2019-01-09 14:49:20 -0600

    still working on redux for cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -10,13 +10,24 @@ const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
 const addToCart = product => ({type: ADD_TO_CART, product})
 const removeFromCart = product => ({type: REMOVE_FROM_CART, product})
 
+/**
+ * INITIAL STATE
+ */
+
+const initialState = {
+  cartProducts: []
+}
+
+/**
+ * REDUCER
+ */
 export default function(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART:
-      return [...state, action.product]
+      return {cartProducts: [...state, action.product]}
     case REMOVE_FROM_CART:
-      ;[action.product, ...remainingItems] = state
-      return remainingItems
+      ;[action.product, ...remainingItems] = state.cartProducts
+      return {cartProducts: [remainingItems]}
     default:
       return state
   }
--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -2,3 +2,10 @@ import {expect} from 'chai'
 import {addToCart, removeFromCart} from './cart'
 
 import configureMockStore from 'redux-mock-store'
+
+const mockStore = configureMockStore
+
+import {createStore} from 'redux'
+import {reducer} from './index'
+
+const initialState = {}
