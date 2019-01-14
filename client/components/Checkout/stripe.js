f02cfc0ea5f6d9236eb504cf3c40c14a392f2b4f
Duc Tran <gtductran@gmail.com>
2019-01-14 08:42:41 -0600

    removed jquery haha

--- client/components/Checkout/stripe.js
+++ client/components/Checkout/stripe.js
@@ -2,32 +2,30 @@ import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 import axios from 'axios'
 
-const stripeBtn = () => {
+const stripeBtn = props => {
   const publishableKey = 'pk_test_DUbKNX2jBsDxZV2i0McVhWe6'
 
   const onToken = token => {
     const body = {
-      amount: 999,
+      amount: props.bill.total,
       token: token
     }
     axios
       .post('http://localhost:8000/payment', body)
       .then(response => {
         console.log(response)
-        alert('Payment Success')
       })
       .catch(error => {
         console.log('Payment Error: ', error)
-        alert('Payment Error')
       })
   }
   return (
     <StripeCheckout
       label="Order"
-      name="Pastabilities" //Modal Header
+      name="Pastabilities"
       description="Pastas heading your way."
       panelLabel="Order"
-      amount={999}
+      amount={props.bill.total}
       token={onToken}
       stripeKey={publishableKey}
     />
