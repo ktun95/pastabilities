0eb627d2666e3807d260398e6c08086b8fb8d102
apoyando <apoyando@yahoo.com>
2019-01-15 16:22:07 -0600

    working on admin still

--- client/components/adminOrders.js
+++ client/components/adminOrders.js
@@ -10,6 +10,7 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import {withStyles} from '@material-ui/core/styles'
+import {fetchOrders} from '../store'
 
 const styles = theme => ({
   productFilter: {
@@ -31,6 +32,7 @@ const styles = theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
+    flexDirection: 'column',
     spacing: 8
   },
   item: {
@@ -40,8 +42,10 @@ const styles = theme => ({
     display: 'flex'
   },
   card: {
-    width: 225,
-    height: '100%'
+    margin: '10px',
+    height: '100%',
+    padding: '10px',
+    flexDirection: 'row'
   },
   cardMedia: {
     paddingTop: '70%'
@@ -56,16 +60,63 @@ const styles = theme => ({
   },
   button: {
     marginBottom: '10px'
+  },
+  paper: {
+    marginTop: '10px'
   }
 })
 class AdminOrders extends React.Component {
+  constructor() {
+    super()
+  }
+  componentDidMount() {
+    this.props.fetchOrders()
+  }
   render() {
+    const {classes, allOrders} = this.props
     return (
-      <Grid container className={classes.container} spacing={16}>
-        ORDERS HERE
-      </Grid>
+      <Paper className={classes.paper}>
+        <Grid container className={classes.container} spacing={16}>
+          <Paper>
+            <Typography variant="h3" align="center">
+              All Orders
+            </Typography>
+          </Paper>
+          {allOrders &&
+            allOrders.map(order => (
+              <Card key={order.id}>
+                <div>Order Id: {order.id}</div>
+                <div>Order Date: {order.orderDate}</div>
+                <div>Status: {order.status}</div>
+              </Card>
+            ))}
+        </Grid>
+      </Paper>
     )
   }
 }
+const mapState = ({product, user}) => {
+  return {
+    products: product.allProducts,
+    isAdmin: user.isAdmin,
+    userId: user.id,
+    currentPage: product.currentPage,
+    numPages: product.numPages,
+    productsPerPage: product.productsPerPage,
+    visibleProducts: product.visibleProducts,
+    types: product.types,
+    shapes: product.shapes
+  }
+}
 
-export default withStyles(styles)(AdminOrders)
+const mapStateToProps = ({order}) => ({
+  allOrders: order.orders
+})
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchOrders: () => dispatch(fetchOrders())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withStyles(styles)(AdminOrders)
+)
